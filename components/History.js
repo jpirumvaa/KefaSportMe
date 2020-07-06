a28915cd5f62290bb96e5dfd2e0c9af3dd79f023
Jean Pierre <jp.irumva@gmail.com>
2020-07-06 18:33:36 +0200

    failed renderItem

---
+++ components/History.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+import {connect} from 'react-redux'
+import {receiveEntries, addEntry} from '../actions'
+import {timeToString, getDailyReminderValue} from '../utils/helpers'
+import {fetchCalendarResults} from '../utils/api'
+//import UdaciFitnessCalendar from 'udacifitness-calendar'
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
+
+class History extends Component {
+    componentDidMount(){
+        const {dispatch}=this.props       
+
+        fetchCalendarResults()
+        .then((entries)=>dispatch(receiveEntries(entries)))
+        .then(({entries})=>{
+            if(!entries[timeToString()]){
+                dispatch(addEntry({
+                    [timeToString()]:getDailyReminderValue()
+                }))
+            }
+        })
+    }
+/*
+    renderItem=({today, ...metrics}, formattedDate, key)=>(
+        <View style={styles.item}>
+            {today?
+            <View>
+            <DateHeader date={formattedDate}/>*
+                <Text style={styles.noDataText}>
+                {today}
+                </Text>
+            </View>
+        : <TouchableOpacity
+            onPress={() => console.log('Pressed!')}
+            >
+                <Text>{JSON.stringify(metrics)}</Text>
+            </TouchableOpacity>}
+        </View>
+
+    )
+            */
+    renderItem=(today, formattedDate)=>(
+        <View>
+            {today?
+            <Text>{JSON.stringify(today)}</Text>:
+            <Text>{JSON.stringify(metrics)}</Text>}
+        </View>
+    )
+
+    /* 
+    renderEmptyDate(formattedDate){
+        return(
+        <View style={styles.item}>
+            <DateHeader date={formattedDate}/>
+            <Text style={styles.noDataText}>
+                You didn't log any data for this day!
+            </Text>
+        </View>
+        )
+    }
+    */
+renderEmptyDate(formattedDate){
+    return(
+        <View>
+            <Text>No Data for this day</Text>
+        </View>
+    )
+}
+    render() {
+        const {entries}= this.props
+        
+        
+        return (
+            <Agenda
+            items={entries}
+            renderItem={this.renderItem}
+            renderEmptyDate={this.renderEmptyDate}
+            />
+
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+    },
+    },
+    noDataText: {
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20
+    }
+    })
+
+const mapStateToProps=(entries)=>{
+    return{
+        entries
+    }
+}
+
+export default connect(mapStateToProps)(History)
